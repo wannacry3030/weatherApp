20498719139397b103c067a2bda12d3022e2a6ca
wannacry3030 <wannacry.bat@proton.me>
2023-05-07 13:18:47 -0300

    update main.js

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 //Openweather API, nao compartilhe com ngm
 const api = "eae2a5e6dc35e18b5696bd1bcbd7e5af";
 
+const iconImg = document.getElementById("weather-icon");
+const loc = document.querySelector("#location");
+const tempC = document.querySelector(".c");
+const tempF = document.querySelector(".f");
+const desc = document.querySelector(".desc");
+const sunriseDOM = document.querySelector(".sunrise");
+const sunsetDOM = document.querySelector(".sunset");
+
 //criando a arrow funtion que vai salvar a latitude e longitude
 window.addEventListener("load", () => {
   let long;
@@ -25,8 +33,24 @@ window.addEventListener("load", () => {
           const { description, icon } = data.weather[0];
           const { sunrise, sunset } = data.sys;
           console.log(data);
+
+          const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+          const fahrenheit = (temp * 9) / 5 + 32;
+
+          //convertendo Epoch(unidade)time para GMT
+          const sunriseGMT = new Date(sunrise * 1000);
+          const sunsetGMT = new Date(sunset * 1000);
+
+          iconImg.src = iconUrl;
+          loc.textContent = `${place}`;
+          desc.textContent = `${description}`;
+          tempC.textContent = `${temp.toFixed(2)} °C`;
+          tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
+          sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()} `;
+          sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
         });
     });
   }
 });
+
 //formula pra converter celsius pra fahrenheit = (celcius * 9) / 5 + 32
