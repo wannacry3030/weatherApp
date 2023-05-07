1af92182868851857e9f58bdd7b2753feb344ed6
wannacry3030 <wannacry.bat@proton.me>
2023-05-07 13:06:45 -0300

    update main.js

--- main.js
+++ main.js
@@ -11,6 +11,22 @@ window.addEventListener("load", () => {
       // Storing Longitude and Latitude in variables
       long = position.coords.longitude;
       lat = position.coords.latitude;
+      base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
+      console.log(base);
+      //usando Fetch para pegar a DATA
+      fetch(base)
+        .then((response) => {
+          //convertendo a resposta para um objeto JSON
+          return response.json();
+        })
+        .then((data) => {
+          const { temp } = data.main;
+          const place = data.name;
+          const { description, icon } = data.weather[0];
+          const { sunrise, sunset } = data.sys;
+          console.log(data);
+        });
     });
   }
 });
+//formula pra converter celsius pra fahrenheit = (celcius * 9) / 5 + 32
